8f5e67a6320be2d758201db5632df758abbc6aad
rawkush <ankushrawat950@gmail.com>
2018-08-22 15:03:02 +0530

    changed the logic of the kangaroo

--- week_1/submissions/rawkush/C++/5.cpp
+++ week_1/submissions/rawkush/C++/5.cpp
@@ -2,15 +2,6 @@
 
 using namespace std;
 
-void swap(int &x1,int &v1,int &x2,int &v2){
-int tempx=x1;
-int tempv=v1;
-x1=x2;
-v1=v2;
-x2=tempx;
-v2=tempv;
-}
-
 
 int main() {
  int x1, v1, x2, v2;
@@ -26,19 +17,12 @@ int main() {
   cout<<"distance per jump(velocity)";
   cin>>v2;
 	
-  if(x1>x2)
-	swap(x1,v1,x2,v2);
- 
-if(x2>x1&& v2>v1)
-{cout<<"no";
-return 0; 
- }
-  //for( int i=0;(x1-x2)>=0;i++){
-  
- dist= (x2*v1)/(v1-v2);
-  cout<< "number of steps = "<<(dist/v1);
-  cout<< "yes" 	;
-   
+
+if((x1 - x2) % (v2 - v1) == 0){
+cout<<"yes";
+cout<<endl<<(x1 - x2) / (v2 - v1);
+}else
+cout<<"no";
 
 return 0;
 }
