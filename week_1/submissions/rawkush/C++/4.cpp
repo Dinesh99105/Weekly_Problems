d9c22fd26044e75e5e3e14f337860c5d495ae60e
rawkush <ankushrawat950@gmail.com>
2018-08-22 13:53:08 +0530

    completing the code

--- week_1/submissions/rawkush/C++/4.cpp
+++ week_1/submissions/rawkush/C++/4.cpp
@@ -29,13 +29,29 @@ return small;
 }
 
 int main(){
-
+int minSum=0,maxSum=0;
 int x[5];
 for(int i=0;i<5;i++)
 cin>>x;
 
+int small,big;
+smallminSum=findMinPosition(x);
+big=findMaxPosition(x);
 
+for(int i=0;i<5;i++){
 
+if( i!=big){
+minSum=minSum+a[i];
+}
+
+if(i!=small){
+maxSum=maxSum+a[i]
+}
+
+}
+
+cout<<"max sum is"<<maxSum;
+cout<<"\nmin sum is"<<minSum;
 
 return 0;
 }
