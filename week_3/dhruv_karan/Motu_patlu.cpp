d57e61fcd5cea423779c2bf8453b746f8f6970c1
dhruv-karan <dhruvkaran24@gmail.com>
2019-01-02 17:10:34 +0530

    generalised but one edge case left

--- week_3/dhruv_karan/Motu_patlu.cpp
+++ week_3/dhruv_karan/Motu_patlu.cpp
@@ -1,30 +1,42 @@
 #include <iostream>
 using namespace std;
 int main() {
-  int ice[5] = {6,4,5,1,5};
-  int sum = 18;
+  int n, input,Lpa ;
+  cout<<"entert the length of array"<<"\n";
+  cin>>n;
+//   int ice[n] = {6,4,5,1,5};
+
+    int ice[n];
+      int sum = 0;
+  for (int i=0 ; i<n;i++){
+    cout<<"enter the num";
+    cin>>input;
+    ice[i] = input;
+    sum +=input;
+    if(i == n-1){
+       cout<<"enter the speed of patu eats";
+       cin>>Lpa;
+    }
+  }
+  int Lmo = Lpa*2;
   int Em=0;
   int Ep=0;
-  // int motu = 2;
-  // int patlu = 1;
-  // int motu_patlu = 2;
-  // int ques = 3;
-  int Lmo = 14;
-  int Lpa = 7;
   int i=0;
+  
   while(Lmo >0 ){
        Em++;
      Lmo = Lmo-ice[i];
+     Lmo <0 ? -1*Lmo : Lmo;
      i++;
   }
   int j=1;
   while(Lpa >0 ){
        Ep++;
-    Lpa = Lpa-ice[5-j];
+    Lpa = Lpa-ice[n-j];
+    Lpa <0 ? -1*Lpa : Lpa;
      j++;
   }
   if(Ep==Em){
-    // cout<<"yes";
     Ep--;
   }
   // cout<<i<<j<<"\n";
